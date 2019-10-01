70a685d02d2d2b7f0e0a2b0d2b60c52b076cf72f
lhossainduet094026 <56044580+lhossainduet094026@users.noreply.github.com>
2019-10-02 01:19:36 +0600

    Add files via upload

---
+++ DCP_85_TomorrowIsEid.java
@@ -0,0 +1,18 @@
+package dcp_85_tomorrow.is.eid;
+
+import java.util.Scanner;
+
+public class DCP_85_TomorrowIsEid {
+
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+       int i = sc.nextInt();
+       if(i==29||i==30)
+       {
+       System.out.println("YES");
+       }
+       else
+           System.out.println("NO");
+    }
+    
+}
