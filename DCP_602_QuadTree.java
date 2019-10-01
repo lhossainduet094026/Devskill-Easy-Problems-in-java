2bff4e3aba95c286c4ba85c4e0af91bcc55ade9c
lhossainduet094026 <56044580+lhossainduet094026@users.noreply.github.com>
2019-10-02 01:29:10 +0600

    Add files via upload

---
+++ DCP_602_QuadTree.java
@@ -0,0 +1,11 @@
+
+package dcp_602_quad.tree;
+
+public class DCP_602_QuadTree {
+
+    public static void main(String[] args) {
+    
+    String s ="Quad Tree";
+    System.out.println(s);
+    }    
+}
