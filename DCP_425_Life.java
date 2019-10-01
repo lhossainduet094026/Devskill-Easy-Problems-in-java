4f761829731df279e08ca4da746797ef26a9f6cb
lhossainduet094026 <56044580+lhossainduet094026@users.noreply.github.com>
2019-10-02 01:25:28 +0600

    Add files via upload

---
+++ DCP_425_Life.java
@@ -0,0 +1,11 @@
+
+package dcp_425_.life;
+
+public class DCP_425_Life {
+
+    public static void main(String[] args) {
+       //int totalAnswerPoints = 9;
+        System.out.println("9");
+    }
+    
+}
